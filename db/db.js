899f9dd6954fa53347a2686f9b0bfa832970ce69
G1LW1N <gilwin.toledo08@gmail.com>
2026-02-22 13:01:40 +0800

    Database configuration

--- db/db.js
+++ db/db.js
@@ -10,6 +10,7 @@ export const db = new Pool(
         ? {
               connectionString: process.env.DATABASE_URL,
               ssl: {
+                  require: true,
                   rejectUnauthorized: false
               }
           }
